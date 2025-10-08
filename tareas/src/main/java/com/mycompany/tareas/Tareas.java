f58fc68d091d70ff455dd666bffd63633201cda5
dan21az <daniel.anzules1248@gmail.com>
2025-10-07 20:59:08 -0500

    changes

---
+++ tareas/src/main/java/com/mycompany/tareas/Tareas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tareas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Tareas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
