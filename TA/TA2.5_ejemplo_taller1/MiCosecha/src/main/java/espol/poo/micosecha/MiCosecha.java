e1277611dabec1fac8f74667855483d42d96b53d
dan21az <daniel.anzules1248@gmail.com>
2025-10-16 22:55:03 -0500

    ta2.5

---
+++ TA/TA2.5_ejemplo_taller1/MiCosecha/src/main/java/espol/poo/micosecha/MiCosecha.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espol.poo.micosecha;
+
+/**
+ *
+ * @author Gladys
+ */
+public class MiCosecha {
+
+    public static void main(String[] args) {
+
+        //completar de acuerdo a las instrucciones
+    }
+}
